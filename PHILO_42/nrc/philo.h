4fec1d2ac7e7352953ca7fff3fae7f80011e4f52
filiXx <walido55x@gmail.com>
2025-08-04 04:48:33 +0100

    creat folders and files for the projects

---
+++ PHILO_42/nrc/philo.h
@@ -0,0 +1,14 @@
+#ifndef PHILO_H
+#define PHILO_H
+
+#include "../../ft_libft/libft.h";
+# include <ctype.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+
+
+
+#endif
---
+++ PHILO_42/src/main.c
@@ -0,0 +1,6 @@
+#include "../nrc/philo.h";
+
+int main()
+{
+    return 0;
+}
